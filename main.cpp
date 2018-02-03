0136951d6899c40642244c0568ee1ef24bfb56eb
Josef Vitu <josef@vitu.org>
2018-02-03 12:23:27 +0100

    Fix code formatting

--- main.cpp
+++ main.cpp
@@ -50,12 +50,13 @@ bool verbose = false;
 void writeElement(const char* name, const TagLib::String& value) {
   if (!value.isEmpty()) {
     xmlTextWriterWriteElement(writer, BAD_CAST name,
-        BAD_CAST value.toCString(true));
+                              BAD_CAST value.toCString(true));
   }
 }
 
 void try_parse(const std::string& name) {
   TagLib::FileRef f(name.c_str(), false);
+
   if (!f.isNull()) {
     std::string short_name(name);
     short_name.erase(0, input_dir.size());
@@ -63,24 +64,30 @@ void try_parse(const std::string& name) {
     xmlTextWriterStartElement(writer, BAD_CAST "file");
 
     xmlTextWriterWriteAttribute(writer, BAD_CAST "name",
-        BAD_CAST short_name.c_str());
+                                BAD_CAST short_name.c_str());
     xmlTextWriterEndAttribute(writer);
 
     const TagLib::Tag* tag = f.tag();
+
     if (!tag->isEmpty()) {
       writeElement("title", tag->title());
       writeElement("artist", tag->artist());
       writeElement("album", tag->album());
+
       char year[5];
       snprintf(year, sizeof(year), "%d", tag->year());
+
       if (strcmp(year, "0")) { // Actually empty
-          writeElement("year", year);
+        writeElement("year", year);
       }
+
       char track[5];
       snprintf(track, sizeof(track), "%d", tag->track());
+
       if (strcmp(track, "0")) { // Actually empty
-          writeElement("track", track);
+        writeElement("track", track);
       }
+
       writeElement("genre", tag->genre());
     }
 
@@ -91,9 +98,9 @@ void try_parse(const std::string& name) {
 int list_dir(const char* name, const struct stat* /*status*/, int type, struct FTW* /*ftwbuf*/) {
   switch (type) {
     case FTW_F:
-        // A file
-        files.push_back(name);
-        break;
+      // A file
+      files.push_back(name);
+      break;
     default:
       break;
   }
@@ -105,6 +112,7 @@ int parse_and_write(const char* input_dir, const char* output_file) {
   xmlDocPtr document;
 
   writer = xmlNewTextWriterDoc(&document, 0);  // No compression
+
   if (writer == NULL) {
     fprintf(stderr, "Error creating the XML writer.\n");
     xmlFreeDoc(document);
@@ -117,8 +125,7 @@ int parse_and_write(const char* input_dir, const char* output_file) {
   // Root element
   xmlTextWriterStartElement(writer, BAD_CAST "collection");
 
-  xmlTextWriterWriteAttribute(writer, BAD_CAST "root",
-      BAD_CAST input_dir);
+  xmlTextWriterWriteAttribute(writer, BAD_CAST "root", BAD_CAST input_dir);
   xmlTextWriterEndAttribute(writer);
 
   // Recursively process the input directory
